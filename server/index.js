8ee5a0bfc9aa766f06c841ba0c9b72b091f53a1e
buntgrp666 <buntgrp666@penguin>
2022-12-22 21:49:19 +0200

    Backend: Created routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const sequelize = require('./db');
-const models = require('./models/models');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const PORT = process.env.PORT || 5000;
+const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
+app.use("/api", router);
+
+// Errors handle
+app.use(errorHandler);
 
 const start = async () => {
   try {
